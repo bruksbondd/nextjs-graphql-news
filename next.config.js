aa722c58f5f4f99fbb3f3db9785154fea2cd061f
Serhii Brekhov <bruksbond@gmail.com>
2023-10-17 12:04:53 +0300

    init and add navbar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
 
-module.exports = nextConfig
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+};
+
+module.exports = nextConfig;
---
+++ src/lib/fetchNews.ts
@@ -0,0 +1,8 @@
+const fetchNews = async (
+  category?: Category | string,
+  keywords?: string,
+  isDynamic?: boolean,
+) => {};
+
+export default fetchNews;
+// stepzen import curl "http://api.mediastack.com/v1/news?access_key=apikey&sources=business,sports"
